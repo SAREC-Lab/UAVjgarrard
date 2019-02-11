e91d9c0b67028c9a3c92daaf7c8bb3dba2a3f64e
JustinUAV <justin.garrard.77@gmail.com>
2019-02-11 15:26:20 -0500

    checkpoint

--- flight_utils.py
+++ flight_utils.py
@@ -13,9 +13,19 @@ import time
 import datetime
 import threading
 
-################################################################################################
-#Logging Utilities
-################################################################################################
+########################################################################
+#Logging & Config Setup Utilities
+########################################################################
+def load_json(path2file):
+    d = None
+    try:
+        with open(path2file) as f:
+            d = json.load(f)
+    except Exception as e:
+        exit('Invalid path or malformed json file! ({})'.format(e))
+
+    return d
+
 def printLog(msg, fname):
   fh = open(fname, 'a')
   fh.write(msg)
@@ -27,15 +37,11 @@ def initLog(fname):
   fh.close()
 
 
+########################################################################
+#This section is utilities for managing the flight of a single drone
+########################################################################
 
-################################################################################################
-# ARM and TAKEOFF
-################################################################################################
-
-# function:   	arm and takeoff
-# parameters: 	target altitude (e.g., 10, 20)
-# returns:	n/a
-
+####Arm and Takeoff for Single Drone####
 def arm_and_takeoff(vehicle, aTargetAltitude):
     """
     Arms vehicle and fly to aTargetAltitude.
@@ -70,12 +76,11 @@ def arm_and_takeoff(vehicle, aTargetAltitude):
             break
         time.sleep(1)
 
+####Get the Coords of a Vehicle####
+def get_location(vehicle):
+    return vehicle.location.global_relative_frame
 
-################################################################################################
-# function:    Get distance in meters
-# parameters:  Two global relative locations
-# returns:     Distance in meters
-################################################################################################
+####Get Distance in Meters####
 def get_distance_meters(locationA, locationB, fname):
     # approximate radius of earth in km
     R = 6373.0
@@ -97,10 +102,35 @@ def get_distance_meters(locationA, locationB, fname):
     	printLog("\tDistance (meters): {}".format(distance, locationA), fname)
     return distance
 
+#######################################################################
+#Functions for managing multiple drones sequentially
+#######################################################################
 
-################################################################################################
-# Threaded Fly to
-################################################################################################
+#Launches multiple drones, could thread later?
+def mult_arm_and_takeoff(drones, alts):
+  for count, drone in enumerate(drones):
+	arm_and_takeoff(drone, alts[count])
+  return 1
+
+	
+#######################################################################
+#Functions for managing multiple drones w/ threads
+#######################################################################
+
+####Launch all fly_to threads####
+def threaded_launch(vehicles): 
+  #Use a thread dictionary so they can be grabbed, and stopped
+  threads = {}
+
+  for i, vehicle in enumerate(vehicles):
+  	name = "UAV-{}.log".format(i)
+  	flight_utils.printLog("{}- starting {}".format(time.time(), name), log_name)
+  	flight_utils.initLog(name)
+	threads[name] = [flight_utils.thread_fly_to(args=(vehicle, routes[i], 10, 1, name, MOREARGS,)), q, i]
+  	threads[name][0].start()
+  return threads
+
+####Threaded Fly to####
 class thread_fly_to(threading.Thread):
     def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, verbose=None):
 	threading.Thread.__init__(self, group=group, target=target, name=name, verbose=verbose)
@@ -109,7 +139,10 @@ class thread_fly_to(threading.Thread):
 	self.groundspeed = args[2]
 	self.dist = args[3]
 	self.fname = args[4]
-	self.q = args[5]
+	self.lowLat = args[5]
+	self.highLat = args[6]
+	self.lowLon = args[7]
+	self.highLon = args[8]
     	self.kwargs = kwargs
 	self.interrupt = 0
 	self.crash = 0
@@ -117,6 +150,14 @@ class thread_fly_to(threading.Thread):
 
     # Fly to
     def fly_to(self, targetLocation):
+        print("Trying to fly: checking geo-boundaries")
+    if (targetLocation.lat < self.lowLat or targetLocation.lat > self.highLat):
+        print("ERROR when assigning location! - Latitude outside range!")
+        return
+    if (targetLocation.lon < self.lowLon or targetLocation.lon > self.highLon):
+        print("ERROR when assigning location! - Longitude outside range!")
+        return
+
     	printLog("Flying to waypoint: {}".format(targetLocation), self.fname)
     	self.vehicle.groundspeed = self.groundspeed
     	currentTargetLocation = targetLocation
@@ -138,7 +179,6 @@ class thread_fly_to(threading.Thread):
 #	  if plotLog:
 #	    plotLog.add_data(vehicle.location.global_relative_frame.lon,vehicle.location.global_relative_frame.lat)
     	printLog("\n", self.fname)
-#	return
 
     def avoid(self, alt, other):
 	self.interrupt = 1
@@ -168,7 +208,6 @@ class thread_fly_to(threading.Thread):
 	    break
 	  elif self.interrupt:
 	    continue
-	  #Somehow I'm getting here with an empty list
 	  elif len(self.route) > 0:
 	    self.fly_to(LocationGlobalRelative(self.route[0][0],self.route[0][1], self.route[0][2]))
 	    if self.interrupt:
@@ -182,6 +221,7 @@ class thread_fly_to(threading.Thread):
     	self.vehicle.close()
 	return
 
+####When central controller detects two drones are w/in 6 meters, monitor them####
 def monitor(thread1, thread2, uav1, uav2):
   thread1[0].avoid(5, thread2[2])
   thread2[0].avoid(15, thread2[2])
@@ -194,6 +234,7 @@ def monitor(thread1, thread2, uav1, uav2):
   thread2[0].resume()
   return
 
+####When central controller detects a drone has crashed, put it down####
 def crash(thread, other):
   thread[0].go_down(other)
   return
--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 #Justin Garrard
-#Homework 3
-
-#TODO
-#Collision Avoidance
-	#What happens if I send a vehicle.goto outside of the thread?
-	#How to kill/restart threads, better enumeration mechanism
+#Homework 4
 
 import dronekit_sitl
 import dronekit
@@ -14,19 +9,9 @@ import os
 import threading, Queue
 import time
 import sys
-import utilities
-from copter import UAV_Copter
+import flight_utils 
 
-def load_json(path2file):
-    d = None
-    try:
-        with open(path2file) as f:
-            d = json.load(f)
-    except Exception as e:
-        exit('Invalid path or malformed json file! ({})'.format(e))
-
-    return d
-
+#parse option for dronology
 if len(sys.argv) < 2 or len(sys.argv) > 3:
   print "Improper use!\n./main.py <1-5> -ca, where 1-5 is the test file and -ca enables avoidance"
   sys.exit() 
@@ -36,65 +21,49 @@ testFile = "./test{}.json".format(sys.argv[1])
 if len(sys.argv) == 3:
   avoid = 1
 
-config = load_json(testFile)
+config = flight_utils.load_json(testFile)
   
-
 # A list of sitl instances.
 sitls = []
 
 # A list of drones. (dronekit.Vehicle)
 vehicles = []
 
-# A list of lists of lists (i.e., [ [ [lat0, lon0, alt0], ...] ...]
 # These are the waypoints each drone must go to!
 routes = []
 
-# This is really temporary for this assignment so we can track IDs for each drone
-# Next week we'll integrate with Dronology and replace it.
-copters = []
-
+###Connect to dronology
 # Start up all the drones specified in the json configuration file
 for i, v_config in enumerate(config):
-    	copter = UAV_Copter()
+#    	copter = UAV_Copter()
     	home = v_config['start']
    	vehicle, sitl = copter.connect_vehicle(i, home)
     	sitls.append(sitl)
     	vehicles.append(vehicle)
     	routes.append(v_config['waypoints'])
     	vehicle_id = str("UAV-" + str(i))
-    	copter.setvalues(sitl, vehicle, v_config['waypoints'], vehicle_id)
-    	copters.append(copter)
-
-#have each vehicle launch to altitude before beginning to fly
-#could be done w/ threads and joins but not as important rn 
-for vehicle in vehicles: 
-  	utilities.arm_and_takeoff(vehicle, 10)
-
-#Use a thread dictionary so they can be grabbed, and stopped
-threads = {}
 
 log_name = "CA-{}Test-{}.log".format(avoid, sys.argv[1])
-utilities.initLog(log_name)
+flight_utils.initLog(log_name)
 
-for i, vehicle in enumerate(vehicles):
-  	name = "UAV-{}.log".format(i)
-  	utilities.printLog("{}- starting {}".format(time.time(), name), log_name)
-  	q = Queue.Queue()
-  	utilities.initLog(name)
-	threads[name] = [utilities.thread_fly_to(args=(vehicle, routes[i], 10, 1, name, q,)), q, i]
-  	threads[name][0].start()
+#Have each vehicle launch to altitude before beginning to fly
+flight_utils.mult_arm_and_takeoff(vehicles, [10] * len(vehicles))
+
+#Begin flight for all vehicles
+threads = flight_utils.threaded_launch(vehicles)
 
 min_separation = 6
 monitorThreads = {}
 crashedUAVs = []
 
 while 1:
+	#Make functions out of these shits
 	msg = ""
 	count = 1
 	for vehicle in vehicles:
 	  msg = msg + "UAV-{}: {}\n\t".format(count, vehicle.location.global_relative_frame)
   	  count += 1
-	utilities.printLog("time-{}\n\t{}".format(time.time(), msg), log_name)
+	flight_utils.printLog("time-{}\n\t{}".format(time.time(), msg), log_name)
 
   	#check all vehicles for collisions
   	for uav in range(len(vehicles)):
@@ -104,23 +73,23 @@ while 1:
 
 	  #To make this more efficient we could remove vehicles when that vehicles thread finishes, but we get indexing issues into the vehicles list
 	  for x in range(uav + 1, len(vehicles)):
-	    dist = utilities.get_distance_meters(vehicles[uav].location.global_relative_frame, vehicles[x].location.global_relative_frame,None)
+	    dist = flight_utils.get_distance_meters(vehicles[uav].location.global_relative_frame, vehicles[x].location.global_relative_frame,None)
 	  
 	    #launch a monitor thread to see when two drones can resume their routes at regular altitude, or if we "crash" down the vehicles
 	    #crash here
 	    if dist <= 4:
 		if uav not in crashedUAVs:
-		  utilities.printLog("**CRASH**time-{}: UAV-{} and UAV-{} have crashed!".format(time.time(), uav, x), log_name)
+		  flight_utils.printLog("**CRASH**time-{}: UAV-{} and UAV-{} have crashed!".format(time.time(), uav, x), log_name)
 		  key1 = "UAV-{}.log".format(uav)
 	  	  print "UAV-{} has finished".format(uav)
-		  t1 = threading.Thread(target=utilities.crash, args=(threads[key1], x,))
+		  t1 = threading.Thread(target=flight_utils.crash, args=(threads[key1], x,))
 		  t1.start()
 		  crashedUAVs.append(uav)
 	  	  del threads[key1]
 		if x not in crashedUAVs:
 		  key2 = "UAV-{}.log".format(x)
 	  	  print "UAV-{} has finished".format(x)
-		  t2 = threading.Thread(target=utilities.crash, args=(threads[key2], uav,))
+		  t2 = threading.Thread(target=flight_utils.crash, args=(threads[key2], uav,))
 		  t2.start()
 		  crashedUAVs.append(x)
 		  del threads[key2]
@@ -139,8 +108,8 @@ while 1:
 		  	continue
 		elif key1 in threads.keys() and key2 in threads.keys():
 		  print("UAV-{} and UAV-{} are too close!".format(uav,x))
-		  utilities.printLog("time-{}: UAV-{} and UAV-{} are about to crash!".format(time.time(),uav, x), log_name)
-		  monitorThreads[key] = threading.Thread(target=utilities.monitor, args=(threads[key1], threads[key2], vehicles[uav], vehicles[x],))
+		  flight_utils.printLog("time-{}: UAV-{} and UAV-{} are about to crash!".format(time.time(),uav, x), log_name)
+		  monitorThreads[key] = threading.Thread(target=flight_utils.monitor, args=(threads[key1], threads[key2], vehicles[uav], vehicles[x],))
 		  monitorThreads[key].start()
 			#change altitude
 
