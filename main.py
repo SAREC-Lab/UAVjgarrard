6cd5ac32d875463653518251efedbf63d6527934
JustinUAV <justin.garrard.77@gmail.com>
2019-03-04 11:19:21 -0500

    think I'm done

--- main.py
+++ main.py
@@ -1,114 +1,42 @@
-#!/usr/bin/env python
 #Justin Garrard
-#Homework 4
+import utils
+from dronekit import VehicleMode, LocationGlobalRelative
 
-import dronekit_sitl
-import dronekit
-import json
-import os
-import threading, Queue
-import time
-import sys
-import flight_utils 
-import classtest
-from ground_control_station import SimpleGCS
 
-#parse option for dronology
-if len(sys.argv) != 2:
-  print "Improper use!\n./main.py <1-6> <1-5> \n\tspecify test file and number of UAVs!"
-  sys.exit() 
+connection_string = None
+sitl = None
+fname = "FlightLog.log"
+utils.initLog(fname)
 
-avoid = 0
+configs = utils.load_json("./test.json")
 
-testFile = "./homework_testcases/test{}.json".format(sys.argv[1])
-config = flight_utils.load_json(testFile)
-#Maybe later config file will be a list of areas to search but for now nah
-config = config[0]
+waypoints = []
+for wps in configs['waypoints']:
+  waypoints.append(LocationGlobalRelative(wps[0], wps[1], wps[2]))
 
-numUAV = len(config["starts"])
-print "{} UAVs".format(numUAV)
-log_name = "SearchBox.log"
-flight_utils.initLog(log_name)
-flight_utils.printLog("{} UAVs found in config file".format(numUAV), log_name)
-#bounds = config["bounds"]
-lLat = classtest.getsouth()
-hLat = classtest.getnorth()
-lLon = classtest.geteast()
-hLon = classtest.getwest()
 
-# These are the waypoints each drone must go to!
-routes = flight_utils.gen_search(lLat, hLat, lLon, hLon, numUAV, log_name)
-flight_utils.printRoute(routes)
+if not connection_string:
+  sitl, connection_string = utils.connect_SITL(fname)
+  vehicle = utils.connect_Vehicle(connection_string, fname)
 
-#This should end the dronology thread
-global DO_CONT
-DO_CONT = 0
+utils.arm_and_takeoff(vehicle, 20)
+utils.printLog("We took off", fname)
 
-# A list of drones. (dronekit.Vehicle)
-vehicles = []
+home = vehicle.location.global_relative_frame
+waypoints.append(home)
 
-ARDUPATH = "/home/uav/git/ardupilot"
-gcs = SimpleGCS(ARDUPATH)
-gcs.connect()
+for wp in waypoints:
+  utils.fly_to(1, vehicle, wp, fname)
+  print "Finished waypoint {}".format(wp)
 
-##Connect to dronology
-# Start up all the drones specified in the json configuration file
-for i, start in enumerate(config['starts']):
-    	home = start 
-   	name = "UAV-" + str(i)
+utils.printLog("Done flying", fname)
 
-	vehicle = gcs.registerDrone(home, name)
-    	vehicles.append(vehicle)
-    	vehicle_id = str("UAV-" + str(i))
+vehicle.mode =  VehicleMode("LAND")
+utils.printLog("Landing", fname)
 
-#Have each vehicle launch to altitude before beginning to fly
-flight_utils.mult_arm_and_takeoff(vehicles, [10] * len(vehicles))
+vehicle.close()
+if sitl is not None:
+  sitl.stop()
 
-#Begin flight for all vehicles
-speed = 10
-dist = 1
-threads = flight_utils.threaded_launch(vehicles, routes, speed, dist, lLat, hLat, lLon, hLon, log_name)
 
-min_separation = 6
-
-box_lat, box_lon = classtest.getboxcoordinates()
-
-#Thread to write vehicles locations to file
-log_thread = flight_utils.log_locs(args=(vehicles, log_name,flight_utils.Location(box_lat, box_lon),))
-log_thread.start()
-
-nearUAV = -1
-while 1:
-	#Check if we've already gotten rid of all UAV threads
-    	if len(threads.keys()) == 0:
-		break
-
-	#Monitor UAV threads
-      	for k in threads.keys():
-		#If a thread has finished running, remove the corresponding vehicle
-		if not threads[k].isAlive():
-	  	  #We should probably kill the vehicles from the list around here too
-	  	  print "{} has finished".format(k)
-	  	  del threads[k]
-	
-	nearUAV = flight_utils.check_box(vehicles, flight_utils.Location(box_lat, box_lon))
-	if nearUAV > -1:
-	  closest, target = flight_utils.drone_found(threads, vehicles, nearUAV, log_name)
-	  #print "Closest: {}".format(closest) 
-	  #print "Targe: {}".format(target) 
-	  #while flight_utils.get_distance_meters(flight_utils.Location(target[0],target[1]), flight_utils.get_location(vehicles[closest]), None) > 10:
-	  	#print flight_utils.get_distance_meters(flight_utils.Location(target[0],target[1]), flight_utils.get_location(vehicles[closest]), None)
-	#	time.sleep(1)
-	#  print "Cleaning up!"
-	  flight_utils.printLog("time-{}\n\tUAV-{} arrived at box!".format(closest), log_name)
-	  time.sleep(25)
-	  flight_utils.printLog("time-{}\n\tUAV-{} and UAV-{} returning home".format(nearUAV, closest), log_name)
-	  
-	#If we have more than 1 UAV we check for collisions
-	#if(len(vehicles) > 1):
-	#  flight_utils.check_collisions(vehicles, threads, avoid, min_separation, log_name)
-
-log_thread.stop()
-print "Execution completed"
-DO_CONT = 0
 
---
+++ utils.py
@@ -0,0 +1,146 @@
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+from math import pi, sin, cos, sqrt, atan2, radians
+import time
+import os
+import json
+
+##############################################################################
+#Start SITL if no connection string specified
+##############################################################################
+def connect_SITL(fname):
+    import dronekit_sitl
+
+    #connection_string = sitl.connection_string()
+    ardupath ="/home/uav/git/ardupilot"
+    home = "41.714870,-86.240956,221,0"
+    print 'home: {}'.format(home)
+    printLog('home: {}'.format(home), fname)
+    sitl_defaults = os.path.join(ardupath, 'Tools', 'autotest', 'default_params', 'copter.parm')
+    sitl_args = ['-I{}'.format(0), '--home', home, '--model', '+', '--defaults', sitl_defaults]
+    sitl = dronekit_sitl.SITL(path=os.path.join(ardupath, 'build', 'sitl', 'bin', 'arducopter'))
+    sitl.launch(sitl_args, await_ready=True)
+
+    tcp, ip, port = sitl.connection_string().split(':')
+    port = str(int(port) + 0 * 10)
+    connection_string = ':'.join([tcp, ip, port])
+    return sitl, connection_string
+
+def connect_Vehicle(connection_string, fname):
+    # Connect to the Vehicle
+    printLog('Connecting to vehicle on: %s' % connection_string, fname)
+    vehicle = connect(connection_string, wait_ready=True)
+    return vehicle
+
+
+##############################################################################
+# ARM and TAKEOFF
+##############################################################################
+def arm_and_takeoff(vehicle, aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print "Basic pre-arm checks"
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print " Waiting for vehicle to initialise..."
+        time.sleep(1)
+
+    print "home: " + str(vehicle.location.global_relative_frame.lat)
+
+    print "Arming motors"
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print " Waiting for arming..."
+        time.sleep(1)
+
+    print "Taking off!"
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+
+    while True:
+        #Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*.95:
+            print "Reached target altitude"
+            break
+        time.sleep(1)
+
+##############################################################################
+# GET DISTANCE BTWN 2 POINTS 
+##############################################################################
+def get_distance_meters(locationA, locationB):
+    # approximate radius of earth in km
+    R = 6373.0
+
+    lat1 = radians(locationA.lat)
+    lon1 = radians(locationA.lon)
+    lat2 = radians(locationB.lat)
+    lon2 = radians(locationB.lon)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = (R * c) * 1000
+
+    return distance
+
+##############################################################################
+#Logging Utilities
+##############################################################################
+def printLog(msg, fname):
+    fh = open(fname, "a")
+    fh.write(msg+"\n")
+    fh.close()
+
+def initLog(fname):
+    fh = open(fname, "w")
+    fh.close()
+
+##############################################################################
+# ARM and TAKEOFF
+##############################################################################
+def load_json(path2file):
+  d = None
+  try:
+    with open(path2file) as f:
+	d = json.load(f)
+  except Exception as e:
+    exit("Invalid path or malformed json: {}".format(e))
+
+  return d
+
+##############################################################################
+#Fly to a point
+##############################################################################
+def fly_to(dist, vehicle, targetLocation, fname):
+  if targetLocation.lat > 41.715368 or targetLocation.lat < 41.714310:
+    printLog("Latitude out of bounds", fname)
+    return -1
+
+  if targetLocation.lon > -86.239746 or targetLocation.lon < -86.243882:
+    printLog("Longitude out of bounds", fname)
+    return -1
+
+  vehicle.groundspeed = 10
+  vehicle.simple_goto(targetLocation)
+  printLog("Flying to {}".format(targetLocation), fname)
+
+  remDistance = get_distance_meters(targetLocation, vehicle.location.global_relative_frame)
+  printLog("\t{} meters away from target".format(remDistance), fname)
+
+  while vehicle.mode.name == "GUIDED":
+    if remDistance < dist:
+	printLog("Arrived at waypoint\n", fname)
+	break
+    time.sleep(1)
+    remDistance = get_distance_meters(targetLocation, vehicle.location.global_relative_frame)
+    printLog("\t{} meters away from target".format(remDistance), fname)
+
+
